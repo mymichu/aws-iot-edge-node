b4f19dc82874e9e48c08b5e98f7302ac307c52c0
Michel Meyer <meyer_michel@outlook.com>
2021-07-30 23:38:39 +0200

    add tftp server

--- tasks/docker.py
+++ tasks/docker.py
@@ -14,7 +14,10 @@ def setup(c):
     
 @task
 def run(c, image="iot-edge-image"):
-    c.run(f'nohup docker run --rm --network=host --rm -v {current_file_dir}/../build/tmp/deploy/images/iot-edge:/tftpboot --name deploy -t deploy_server:latest')
+    c.run(f'nohup docker run --network=host --rm -v {current_file_dir}/../build/tmp/deploy/images/iot-edge:/tftpboot --name deploy -t deploy_server:latest')
+
+def run_http(c):
+    c.run(f"docker run -dit --name my-apache-app -p 8080:80 -v {current_file_dir}/../build/tmp/deploy/images/iot-edge:/tftpboot:/usr/local/apache2/htdocs/images httpd:2.4")
 
 @task
 def clean(c, image="iot-edge-image"):
